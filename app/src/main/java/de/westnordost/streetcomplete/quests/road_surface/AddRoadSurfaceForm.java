83595f5600bae7ba05eb59d83f3269fb603b820c
Mateusz Konieczny <matkoniecz@gmail.com>
2017-07-01 00:32:35 +0200

    for detail paved show paving_stones in the first line

--- app/src/main/java/de/westnordost/streetcomplete/quests/road_surface/AddRoadSurfaceForm.java
+++ app/src/main/java/de/westnordost/streetcomplete/quests/road_surface/AddRoadSurfaceForm.java
@@ -10,8 +10,8 @@ public class AddRoadSurfaceForm extends RoadSurfaceForm {
 				new RoadSurfaceForm.Surface("paved", R.drawable.panorama_surface_paved, R.string.quest_surface_value_paved, new RoadSurfaceForm.Surface[]{
 						new RoadSurfaceForm.Surface("asphalt", R.drawable.surface_asphalt, R.string.quest_surface_value_asphalt),
 						new RoadSurfaceForm.Surface("concrete", R.drawable.surface_concrete, R.string.quest_surface_value_concrete),
-						new RoadSurfaceForm.Surface("sett", R.drawable.surface_sett, R.string.quest_surface_value_sett),
 						new RoadSurfaceForm.Surface("paving_stones", R.drawable.surface_paving_stones, R.string.quest_surface_value_paving_stones),
+						new RoadSurfaceForm.Surface("sett", R.drawable.surface_sett, R.string.quest_surface_value_sett),
 						new RoadSurfaceForm.Surface("cobblestone", R.drawable.surface_cobblestone, R.string.quest_surface_value_cobblestone),
 						new RoadSurfaceForm.Surface("wood", R.drawable.surface_wood, R.string.quest_surface_value_wood),
 				}),
--- app/src/main/java/de/westnordost/streetcomplete/quests/road_surface/DetailPavedRoadSurfaceForm.java
+++ app/src/main/java/de/westnordost/streetcomplete/quests/road_surface/DetailPavedRoadSurfaceForm.java
@@ -8,8 +8,8 @@ public class DetailPavedRoadSurfaceForm extends RoadSurfaceForm {
 		return new RoadSurfaceForm.Surface[]{
 				new RoadSurfaceForm.Surface("asphalt", R.drawable.surface_asphalt, R.string.quest_surface_value_asphalt),
 				new RoadSurfaceForm.Surface("concrete", R.drawable.surface_concrete, R.string.quest_surface_value_concrete),
-				new RoadSurfaceForm.Surface("sett", R.drawable.surface_sett, R.string.quest_surface_value_sett),
 				new RoadSurfaceForm.Surface("paving_stones", R.drawable.surface_paving_stones, R.string.quest_surface_value_paving_stones),
+				new RoadSurfaceForm.Surface("sett", R.drawable.surface_sett, R.string.quest_surface_value_sett),
 				new RoadSurfaceForm.Surface("cobblestone", R.drawable.surface_cobblestone, R.string.quest_surface_value_cobblestone),
 				new RoadSurfaceForm.Surface("wood", R.drawable.surface_wood, R.string.quest_surface_value_wood),
 				new RoadSurfaceForm.Surface("unpaved", R.drawable.panorama_surface_unpaved, R.string.quest_surface_value_unpaved, new RoadSurfaceForm.Surface[]{
