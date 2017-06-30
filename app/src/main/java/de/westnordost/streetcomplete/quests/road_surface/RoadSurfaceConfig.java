3458700e69b72f01e2f6ef5b21010d728ac30521
Mateusz Konieczny <matkoniecz@gmail.com>
2017-07-01 00:30:56 +0200

    include highway=steps in surface

--- app/src/main/java/de/westnordost/streetcomplete/quests/road_surface/RoadSurfaceConfig.java
+++ app/src/main/java/de/westnordost/streetcomplete/quests/road_surface/RoadSurfaceConfig.java
@@ -7,6 +7,6 @@ public class RoadSurfaceConfig {
 			"trunk","trunk_link","motorway","motorway_link",
 			"primary", "primary_link", "secondary", "secondary_link", "tertiary", "tertiary_link",
 			"unclassified", "residential", "bicycle_road", "living_street", "pedestrian",
-			"track", "road", "service", "footway", "cycleway", "path"
+			"track", "road", "service", "footway", "cycleway", "path", "steps",
 	};
 }
