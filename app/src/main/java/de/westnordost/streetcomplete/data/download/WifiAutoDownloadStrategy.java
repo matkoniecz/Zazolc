248936078b5fc3382b359cd04cfee412ce570217
Mateusz Konieczny <matkoniecz@gmail.com>
2018-05-31 11:51:35 +0200

    mass download with available wifi

--- app/src/main/java/de/westnordost/streetcomplete/data/download/WifiAutoDownloadStrategy.java
+++ app/src/main/java/de/westnordost/streetcomplete/data/download/WifiAutoDownloadStrategy.java
@@ -21,10 +21,7 @@ public class WifiAutoDownloadStrategy extends AActiveRadiusStrategy
 	/** Let's assume that if the user is on wifi, he is either at home, at work, in the hotel, at a
 	 *  café,... in any case, somewhere that would act as a "base" from which he can go on an
 	 *  excursion. Let's make sure he can, even if there is no or bad internet.
-	 *
-	 *  Since download size is almost unlimited, we can be very generous here.
-	 *  However, Overpass is as limited as always, so the number of quest types we download is
-	 *  limited as before */
+     */
 
 	@Override public int getQuestTypeDownloadCount()
 	{
@@ -33,7 +30,7 @@ public class WifiAutoDownloadStrategy extends AActiveRadiusStrategy
 
 	@Override protected int getMinQuestsInActiveRadiusPerKm2()
 	{
-		return 50;
+		return 500;
 	}
 
 	@Override protected int[] getActiveRadii()
