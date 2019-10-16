908712a07802f344c0e82f28221a67d170a4211a
Mateusz Konieczny <matkoniecz@gmail.com>
2019-10-16 17:13:47 +0200

    collect results into a list

--- scan.kt
+++ scan.kt
@@ -1,6 +1,11 @@
 import java.io.File
 import java.io.InputStream
 
+class LicenceData(val licence: String, val file: String, val source: String) {}
+
+fun validLicences() : Array<String> {
+    return arrayOf("Public Domain", "CC0", "CC-BY-SA 1.0", "CC-BY-SA 2.0", "CC-BY-SA 2.5", "CC-BY-SA 3.0", "CC-BY-SA 4.0", "CC-BY 2.0", "CC-BY 3.0", "CC-BY 4.0")
+}
 
 fun main(args: Array<String>) {
     val firstLocation = "/home/mateusz/Documents/StreetComplete/res/authors.txt" // some noxious to process
@@ -8,28 +13,23 @@ fun main(args: Array<String>) {
     val thirdLocation = "/home/mateusz/Documents/StreetComplete/app/src/main/res/authors.txt"
     val inputStream: InputStream = File(thirdLocation).inputStream()
     val inputString = inputStream.bufferedReader().use { it.readText() }
+    val knownLicenced = mutableListOf<LicenceData>()
     for(line in inputString.split("\n").drop(3)) { // remove header lines
         if(line.length == 0) {
             continue
         }
         var licenceFound: String? = null
-        for(licence in arrayOf("Public Domain", "CC0", "CC-BY-SA 1.0", "CC-BY-SA 2.0", "CC-BY-SA 2.5", "CC-BY-SA 3.0", "CC-BY-SA 4.0", "CC-BY 2.0", "CC-BY 3.0", "CC-BY 4.0")) {
+        for(licence in validLicences()) {
             val splitted = line.split(licence)
             if(splitted.size == 2) {
-                println(line)
-                println(licence)
-                println(splitted[0].trim())
-                println(splitted[1].trim())
+                val file = splitted[0].trim()
+                val source = splitted[1].trim()
                 licenceFound = licence
+                knownLicenced += LicenceData(licence, file, source)
             }
         }
         if (licenceFound == null) {
-                println("-------------------------")
-                println(line)
-                println(line)
-                println(line)
-                println(line)
-                println(line)
+                throw Exception("unexpected licence in the input file was encountered, on " + line)
         }
     }
 } 
