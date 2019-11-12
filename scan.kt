8f506388b734bb49e631c8cd76ac11bd7f74f189
Mateusz Konieczny <matkoniecz@gmail.com>
2019-11-12 23:07:46 +0100

    list files without matching licences

--- scan.kt
+++ scan.kt
@@ -3,11 +3,21 @@ import java.io.InputStream
 
 class LicenceData(val licence: String, val file: String, val source: String) {}
 
+class MediaFile(val filePath: File) {}
+
 fun validLicences() : Array<String> {
     return arrayOf("Public Domain", "CC0", "CC-BY-SA 1.0", "CC-BY-SA 2.0", "CC-BY-SA 2.5", "CC-BY-SA 3.0", "CC-BY-SA 4.0", "CC-BY 2.0", "CC-BY 3.0", "CC-BY 4.0")
 }
 
-fun main(args: Array<String>) {
+fun publicDomainAsSimpleShapesFilenames() : Array<String> {
+    return arrayOf("speech_bubble_top.9.png", "speech_bubble_start.9.png", "speech_bubble_end.9.png", "crosshair_marker.png", "location_direction.png", "building_levels_illustration_bg_left.png", "building_levels_illustration_bg_right.png")
+}
+
+fun cutoff() : Int {
+    return 28
+}
+
+fun licencedMedia() : MutableList<LicenceData> {
     val firstLocation = "/home/mateusz/Documents/StreetComplete/res/authors.txt" // some noxious to process
     val secondLocation = "/home/mateusz/Documents/StreetComplete/app/src/main/assets/authors.txt" //bunch of special cases
     val thirdLocation = "/home/mateusz/Documents/StreetComplete/app/src/main/res/authors.txt"
@@ -30,6 +40,76 @@ fun main(args: Array<String>) {
         }
         if (licenceFound == null) {
                 throw Exception("unexpected licence in the input file was encountered, on " + line)
+        }
+    }
+    return knownLicenced
+}
+
+fun mediaNeedingLicenes() : MutableList<MediaFile> {
+    val mediaFiles = mutableListOf<MediaFile>()
+    File("/home/mateusz/Documents/StreetComplete/app/src/main/res").walkTopDown().forEach {
+        if(it.getName().contains(".")) {
+            val split_extension = it.getName().split(".")
+            if(split_extension.size > 1) {
+                var extension = split_extension.last()
+                if(extension !in listOf("yaml", "yml", "xml", "txt")) {
+                    mediaFiles += MediaFile(it)
+                }
+            }
+        }
+    }
+    return mediaFiles
+}
+fun truncatedfileMatchesLicenceDeclaration(fileName : String, licencedFile : String, truncationLength: Int): Boolean {
+    var truncatedFile = fileName
+    if(truncatedFile.length >= truncationLength ) {
+        truncatedFile = truncatedFile.substring(0, truncationLength)
+    }
+    var truncatedLicence = licencedFile
+    if(truncatedLicence.length >= truncationLength ) {
+        truncatedLicence = truncatedLicence.substring(0, truncationLength)
+    }
+    return truncatedFile.equals(truncatedLicence)
+}
+
+fun fileMatchesLicenceDeclaration(fileName : String, licencedFile : String): Boolean {
+    for(delta in listOf(0, 1, 2)) {
+        if (truncatedfileMatchesLicenceDeclaration(fileName, licencedFile, cutoff() - delta) ) {
+            return true
+        }
+    }
+    return false
+}
+
+fun main(args: Array<String>) {
+    val knownLicenced = licencedMedia()
+    val mediaFiles = mediaNeedingLicenes()
+    val usedLicenced = mutableListOf<LicenceData>()
+    for(file in mediaFiles) {
+        var matched = false
+        for(licenced in knownLicenced) {
+            if(fileMatchesLicenceDeclaration(file.filePath.getName(), licenced.file)) {
+                if(matched) {
+                    println(file.filePath.toString() + " matched to " + licenced.file + " but was matched already!")
+                } else {
+                    matched = true
+                    usedLicenced += licenced
+                }
+                //println(file.filePath.toString() + " matched to " + licenced.file)
+            }
+        }
+        if(!matched){
+            val name = file.filePath.getName()
+            if(name !in publicDomainAsSimpleShapesFilenames()) {
+                println(file.filePath.toString() + " remained unmatched")
+                println(name + " remained unmatched")
+                println()
+            }
+        }
+    }
+    for(licenced in knownLicenced) {
+        if(licenced !in usedLicenced) {
+            println(licenced.file + " appears to be unused")
         }
     }
 } 
