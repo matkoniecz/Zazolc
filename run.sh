8ab158ea469fb59b051d640bb5a3ad3deba000d3
Mateusz Konieczny <matkoniecz@gmail.com>
2019-12-05 09:05:04 +0100

    add repo cleaning

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
+cd /home/mateusz/Documents/Archiwum/StreetComplete-ngi-zero
+git add --all
+git commit -m "thrown away"
+git reset --hard @~
+git switch master
+git pull upstream-westnordost master
+cd /home/mateusz/Desktop/streetcomplete_licence_chore
 java -jar out.jar > licence_data.rb
 ruby adder.rb
 cd /home/mateusz/Documents/Archiwum/StreetComplete-ngi-zero
