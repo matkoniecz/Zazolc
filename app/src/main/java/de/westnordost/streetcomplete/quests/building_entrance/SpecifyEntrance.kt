7a3202ea96c907b3628404e18b216b61fc4e222d
Mateusz Konieczny <matkoniecz@gmail.com>
2022-05-31 09:48:25 +0200

    simplify test quest and catch more entrance=yes
    https://github.com/streetcomplete/StreetComplete/issues/2706

--- app/src/main/java/de/westnordost/streetcomplete/quests/building_entrance/SpecifyEntrance.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/building_entrance/SpecifyEntrance.kt
@@ -5,28 +5,16 @@ import de.westnordost.streetcomplete.data.elementfilter.toElementFilterExpressio
 import de.westnordost.streetcomplete.data.osm.mapdata.Element
 import de.westnordost.streetcomplete.data.osm.mapdata.MapDataWithGeometry
 import de.westnordost.streetcomplete.data.osm.osmquests.OsmElementQuestType
+import de.westnordost.streetcomplete.data.osm.osmquests.OsmFilterQuestType
 import de.westnordost.streetcomplete.data.osm.osmquests.Tags
 import de.westnordost.streetcomplete.data.user.achievements.QuestTypeAchievement.PEDESTRIAN
 
-class SpecifyEntrance : OsmElementQuestType<EntranceAnswer> {
+class SpecifyEntrance : OsmFilterQuestType<EntranceAnswer>() {
 
-    private val withoutEntranceFilter by lazy { """
-        nodes with
+    override val elementFilter = """
+         nodes with
           entrance=yes and !barrier and noexit != yes and access != private
-    """.toElementFilterExpression() }
-
-    private val buildingWaysFilter by lazy { """
-        ways, relations with building and building !~ yes|no|service|shed|house|detached|terrace|semi|semidetached_house|roof
-    """.toElementFilterExpression() }
-
-    private val incomingWaysFilter by lazy { """
-        ways with
-          highway ~ path|footway and area != yes and access !~ private|no
-    """.toElementFilterExpression() }
-
-    private val excludedWaysFilter by lazy { """
-        ways with (tunnel and tunnel != no) or covered=yes
-    """.toElementFilterExpression() }
+    """
 
     override val changesetComment = "Add entrance info"
     override val wikiLink = "Key:entrance"
@@ -34,32 +22,6 @@ class SpecifyEntrance : OsmElementQuestType<EntranceAnswer> {
     override val questTypeAchievements = listOf(PEDESTRIAN)
 
     override fun getTitle(tags: Map<String, String>) = R.string.quest_building_entrance_title
-
-    override fun getApplicableElements(mapData: MapDataWithGeometry): Iterable<Element> {
-        val buildingsWayNodeIds = mutableSetOf<Long>()
-        mapData.ways
-            .filter { buildingWaysFilter.matches(it) }
-            .flatMapTo(buildingsWayNodeIds) { it.nodeIds }
-
-        val incomingWayNodeIds = mutableSetOf<Long>()
-        mapData.ways
-            .filter { incomingWaysFilter.matches(it) }
-            .flatMapTo(incomingWayNodeIds) { it.nodeIds }
-
-        val excludedWayNodeIds = mutableSetOf<Long>()
-        mapData.ways
-            .filter { excludedWaysFilter.matches(it) }
-            .flatMapTo(excludedWayNodeIds) { it.nodeIds }
-
-        return mapData.nodes
-            .filter { it.id in buildingsWayNodeIds && it.id in incomingWayNodeIds
-                && it.id !in excludedWayNodeIds
-                && withoutEntranceFilter.matches(it)
-            }
-    }
-
-    override fun isApplicableTo(element: Element): Boolean? =
-        if (!withoutEntranceFilter.matches(element)) false else null
 
     override fun createForm() = AddEntranceForm()
 
