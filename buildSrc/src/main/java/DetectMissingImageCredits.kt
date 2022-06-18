50a7e854beeb0a2fdac2b5a7737d875521f17dc6
Mateusz Konieczny <matkoniecz@gmail.com>
2022-06-18 16:47:01 +0200

    iterate

--- buildSrc/src/main/java/DetectMissingImageCredits.kt
+++ buildSrc/src/main/java/DetectMissingImageCredits.kt
@@ -1,6 +1,3 @@
-@file:JvmName("MyScript")
-@file:CompilerOptions("-jvm-target", "11")
-@file:Repository("https://repo.maven.apache.org/maven2")
 // @file:DependsOn("com.example:library:1.2.3")
 
 import java.io.File
