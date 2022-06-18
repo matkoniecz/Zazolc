f5f9ad423a51da68373f0ccb416106307cf5b259
Mateusz Konieczny <matkoniecz@gmail.com>
2022-06-18 16:43:01 +0200

    variant

--- buildSrc/src/main/java/DetectMissingImageCreditsCI.main.kts
+++ buildSrc/src/main/java/DetectMissingImageCreditsCI.main.kts
@@ -1,6 +1,6 @@
-@file:JvmName("MyScript")
-@file:CompilerOptions("-jvm-target", "11")
-@file:Repository("https://repo.maven.apache.org/maven2")
+//@file:JvmName("MyScript")
+//@file:CompilerOptions("-jvm-target", "11")
+//@file:Repository("https://repo.maven.apache.org/maven2")
 @file:Import("DetectMissingImageCredits.kt")
 // @file:DependsOn("com.example:library:1.2.3")
 
