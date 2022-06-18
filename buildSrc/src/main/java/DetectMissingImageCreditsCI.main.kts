868ba05217cea3bbf87e4291e1c1042c18378f97
Mateusz Konieczny <matkoniecz@gmail.com>
2022-06-18 16:37:22 +0200

    try to import

--- buildSrc/src/main/java/DetectMissingImageCreditsCI.main.kts
+++ buildSrc/src/main/java/DetectMissingImageCreditsCI.main.kts
@@ -1,9 +1,9 @@
 @file:JvmName("MyScript")
 @file:CompilerOptions("-jvm-target", "11")
 @file:Repository("https://repo.maven.apache.org/maven2")
+@file:Import("DetectMissingImageCredits.kt")
 // @file:DependsOn("com.example:library:1.2.3")
 
-//@file:Import("DetectMissingImageCredits.kt")
 
 System.out.println("out")
 System.err.println("err")
