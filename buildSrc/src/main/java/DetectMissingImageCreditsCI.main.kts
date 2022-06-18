3182ac004b3b7090f99f0f39550e8a5804defca4
Mateusz Konieczny <matkoniecz@gmail.com>
2022-06-18 15:47:38 +0200

    wat

--- buildSrc/src/main/java/DetectMissingImageCreditsCI.main.kts
+++ buildSrc/src/main/java/DetectMissingImageCreditsCI.main.kts
@@ -3,7 +3,7 @@
 @file:Repository("https://repo.maven.apache.org/maven2")
 // @file:DependsOn("com.example:library:1.2.3")
 
-@file:Import("DetectMissingImageCredits.kt")
+//@file:Import("DetectMissingImageCredits.kt")
 
 System.out.println("out")
 System.err.println("err")
