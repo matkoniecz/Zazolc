8305ff55fe5f738c489b3614b1ab0d89485f4eb6
Mateusz Konieczny <matkoniecz@gmail.com>
2022-07-27 14:35:39 +0200

    describe program itself

--- buildSrc/src/main/java/UpdateTaginfoListingTask.kt
+++ buildSrc/src/main/java/UpdateTaginfoListingTask.kt
@@ -125,6 +125,12 @@ This is the third attempt, it works, but code likely can be far better.
 // https://github.com/2bad2furious/kotlinx-ast-demo
 // https://github.com/peternewman/StreetComplete/blob/a388043854bf04545dfbc0beb7decda5208a750e/.github/generate-quest-metadata.main.kts
 
+/*  Generate Taginfo tag listing - only tags added or edited are listed
+ *  Tags removed or used in filtering are NOT listed.
+ *
+ *  Follows https://wiki.openstreetmap.org/wiki/Taginfo/Projects documentation
+ */ 
+
 @OptIn(ExperimentalSerializationApi::class) // needed by explicitNulls = false
 open class UpdateTaginfoListingTask : DefaultTask() {
 
