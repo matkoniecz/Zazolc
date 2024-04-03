53b2335d97aa574319360eba50711bac310edf15
Mateusz Konieczny <matkoniecz@gmail.com>
2024-04-03 07:36:19 +0200

    add ZZM to bicycle parking operators

--- app/src/main/java/de/westnordost/streetcomplete/quests/bicycle_parking_operator/AddBicycleParkingOperatorForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/bicycle_parking_operator/AddBicycleParkingOperatorForm.kt
@@ -2,7 +2,7 @@ package de.westnordost.streetcomplete.quests.bicycle_parking_operator
 import de.westnordost.streetcomplete.quests.ANameWithSuggestionsForm
 
 class AddBicycleParkingOperatorForm : ANameWithSuggestionsForm<String>() {
-    override val suggestions: List<String> get() = listOf("ZTP Kraków")
+    override val suggestions: List<String> get() = listOf("ZTP Kraków", "ZZM Kraków")
 
     override fun onClickOk() {
         applyAnswer(name!!)
