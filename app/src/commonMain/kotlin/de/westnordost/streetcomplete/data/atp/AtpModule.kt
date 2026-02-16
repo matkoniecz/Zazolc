0714b58033b21fd67b9ae4ce505e316873be8e85
Mateusz Konieczny <matkoniecz@gmail.com>
2026-02-16 19:55:24 +0100

    make linter shut up about constant

--- app/src/commonMain/kotlin/de/westnordost/streetcomplete/data/atp/AtpModule.kt
+++ app/src/commonMain/kotlin/de/westnordost/streetcomplete/data/atp/AtpModule.kt
@@ -2,11 +2,12 @@ package de.westnordost.streetcomplete.data.atp
 
 import org.koin.dsl.module
 
+const val OSM_ATP_COMPARISON_API_BASE_URL = "https://bbox-filter-for-atp.bulwersator-cloudflare.workers.dev/api/"
+
 val atpModule = module {
     factory { AtpDao(get()) }
     factory { AtpDownloader(get(), get()) }
     // TODO API: connect to actual bidirectional API
-    val OSM_ATP_COMPARISON_API_BASE_URL = "https://bbox-filter-for-atp.bulwersator-cloudflare.workers.dev/api/"
     factory { AtpApiClient(get(), OSM_ATP_COMPARISON_API_BASE_URL, get()) }
     factory { AtpApiParser() }
 
